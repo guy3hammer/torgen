dcf28f565e1fd230f3baae942973e8691949ccaf
guy3hammer <guy3hammer@gmail.com>
2025-03-16 15:18:54 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -139,7 +139,10 @@ export default function HeroSection() {
         </div>
       </div>
 
-
+      <div
+        ref={ref}
+        className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
+      >
         <div
           className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
             inView ? "before:animate-image-glow" : ""
@@ -164,6 +167,7 @@ export default function HeroSection() {
             className="block relative w-full h-full  rounded-[inherit] border object-contain dark:hidden"
           />
         </div>
+	</div>
 	    
     </section>
   );
