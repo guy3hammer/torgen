fe462c6afb8f8c94baa00b9109fad337df33b117
guy3hammer <guy3hammer@gmail.com>
2025-03-16 18:38:20 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -22,9 +22,7 @@ export default function HeroSection() {
       </h1>
 
       <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-        Take control of your destiny.</p>
-      <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-	Adopt an <i>OSAI-First</i> strategy.</p>
+        Control of your destiny. Adopt an <i>OSAI-First</i> strategy.</p>
 
           <div className="mt-6">
             <ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-white">
