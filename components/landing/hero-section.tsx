6b7f53f90cc3f3cd097a6008640fa39965116ea9
guy3hammer <guy3hammer@gmail.com>
2025-03-16 16:27:59 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -20,7 +20,7 @@ export default function HeroSection() {
         Open Source AI is
         <br className="hidden md:block" /> Changing the Game
       </h1>
-      <p><strong><i>Resist Proprietary Lock-in</i></strong></p>
+
       <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
         Take control your destiny by adopting an
         <br className="hidden md:block" /> <i>OSAI-First</i> strategy for your organization
@@ -113,6 +113,12 @@ export default function HeroSection() {
         ref={ref}
         className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
       >
+
+      <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+        Implement cutting-edge use cases for pennies on the dollar
+        <br className="hidden md:block" /> <i>OSAI-First</i> strategy for your organization
+      </p>
+	      
         <div
           className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
             inView ? "before:animate-image-glow" : ""
