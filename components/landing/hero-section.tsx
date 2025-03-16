dde52bafcac2456fb77586dfcc2a6fe055aeb230
guy3hammer <guy3hammer@gmail.com>
2025-03-16 18:28:18 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -240,7 +240,25 @@ export default function HeroSection() {
           />
         </div>
 	</div>
-	    
+
+      <div className="mx-auto flex max-w-screen-xl flex-col gap-8 px-4 py-14 md:px-8">
+        <div className="mx-auto max-w-5xl text-center">
+          <h4 className="text-xl font-bold tracking-tight text-black dark:text-white">
+            Pricing
+          </h4>
+
+          <h2 className="text-5xl font-bold tracking-tight text-black dark:text-white sm:text-6xl">
+            Simple pricing for everyone.
+          </h2>
+
+          <p className="mt-6 text-xl leading-8 text-black/80 dark:text-white">
+            Choose an <strong>affordable plan</strong> that&apos;s packed with
+            the best features for engaging your audience, creating customer
+            loyalty, and driving sales.
+          </p>
+        </div>
+	</div>
+ 
     </section>
   );
 }
