cf7c1ebfb5d50773c447d3043434cfa789222982
guy3hammer <guy3hammer@gmail.com>
2025-03-16 16:38:23 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -108,17 +108,17 @@ export default function HeroSection() {
 		    
             </ul>
           </div>
+
+	              <h2 className="text-center text-sm font-semibold text-gray-600">
+            Empower your team with cutting-edge AI
+        <br className="hidden md:block" /> use cases for pennies on the dollar
+          </h2>
       
       <div
         ref={ref}
         className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
       >
-
-      <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-        Implement cutting-edge use cases for pennies on the dollar
-        <br className="hidden md:block" /> <i>OSAI-First</i> strategy for your organization
-      </p>
-	      
+      
         <div
           className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
             inView ? "before:animate-image-glow" : ""
