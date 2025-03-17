074bf8b491b3724817c26d8cbcc60691d863f360
guy3hammer <guy3hammer@gmail.com>
2025-03-17 09:28:01 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -246,69 +246,7 @@ export default function HeroSection() {
         </div>
 	</div>
 
-	<div class="mx-auto flex max-w-screen-xl flex-col gap-8 px-4 py-14 md:px-8">
-		<div class="mx-auto max-w-5xl text-center">
-			
-			<h3 class="text-5xl font-bold tracking-tight text-black dark:text-white sm:text-6xl">
-				World Class Resources
-			</h3>
 
-			<p class="mt-6 text-xl leading-8 text-black/80 dark:text-white">
-				Torgen's <i>Open-Source-first</i> approach to AI transformation is driven by US citizens with a proven track record for innovation spanning finance, healthcare, manufacturing, logistics, retail/e-comm, digital media and SaaS
-			</p>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-			<div><h4 class="text-xl font-bold tracking-tight text-black dark:text-white">Flexible Solutions for Companies of All Sizes </h4></div>
-
-			
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Workshop/Bootcamp</strong></h2></span>
-				 </TextShimmer>
-				</div><br></br>
-				<p><span><i>practical open source centric use case training for</i></span></p>
-				<p><span><i>technical and non-technical audiences</i></span></p>
-
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Strategy Consulting</strong></h2></span>
-				 </TextShimmer>
-				</div><br></br>
-				<p><span><i>alignment with future state drivers, use case</i></span></p>
-				<p><span><i>productivity/cost-savings analysis, roadmap</i></span></p>
-				
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Security Assessment</strong></h2></span>
-				 </TextShimmer>
-				</div><br></br>
-				<p><span><i>data security, identity/access, logging, governance,</i></span></p>
-				<p><span><i>regulatory compliance, risk/gap analysis</i></span></p>
-
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Project Consulting</strong></h2></span>
-				 </TextShimmer>
-				</div><br></br>
-				<p><span><i>mobile/ai-first app dev; predictive modeling; data viz;</i></span></p>
-				<p><span><i>legacy code modernization, re-arch, re-platform, migration</i></span></p>
-
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Innovation Lab</strong></h2></span>
-				 </TextShimmer>
-				</div><br></br>
-				<p><span><i>private managed AI testing environment for new model</i></span></p>
-				<p><span><i>benchmarking, use case testing and rapid prototyping</i></span></p>
-
-		</div> 	
     </section>
   );
 }
