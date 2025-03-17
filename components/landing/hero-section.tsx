083de8c27c195e31d5e8c914a99e797d19f3957d
guy3hammer <guy3hammer@gmail.com>
2025-03-17 15:10:21 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -151,38 +151,7 @@ export default function HeroSection() {
             className="block relative w-full h-full  rounded-[inherit] border object-contain dark:hidden"
           />
         </div>
-      </div>
-
-      <div
-        ref={ref}
-        className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
-      >
-        <div
-          className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
-            inView ? "before:animate-image-glow" : ""
-          }`}
-        >
-          <BorderBeam
-            size={200}
-            duration={12}
-            delay={11}
-            colorFrom="var(--color-one)"
-            colorTo="var(--color-two)"
-          />
-
-          <img
-            src="/itops-dark.png"
-            alt="IT Ops Image"
-            className="hidden relative w-full h-full rounded-[inherit] border object-contain dark:block"
-          />
-          <img
-            src="/itops-light.png"
-            alt="IT Ops Image"
-            className="block relative w-full h-full  rounded-[inherit] border object-contain dark:hidden"
-          />
-        </div>
-	</div>
-	        
+      </div>        
 
     </section>
   );
