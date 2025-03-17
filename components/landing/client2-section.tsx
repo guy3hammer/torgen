83bb725f2eab67bb9e658eb46306747b537c50a2
guy3hammer <guy3hammer@gmail.com>
2025-03-17 17:18:38 -0400

    Create client2-section.tsx

---
+++ components/landing/client2-section.tsx
@@ -0,0 +1,27 @@
+export default function ClientSection() {
+  return (
+    <section
+      id="clients"
+      className="text-center mx-auto max-w-[80rem] px-6 md:px-8"
+    >
+      <div className="py-14">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+          <h2 className="text-center text-sm font-semibold text-gray-600">
+            CONNECT ANYTHING TO EVERYTHING
+          </h2>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>         
+          <div className="mt-6">
+            <ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-white">
+              
+
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
