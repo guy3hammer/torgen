649605d599f8c05a1b78a86ec1d1f9bd824733f5
guy3hammer <guy3hammer@gmail.com>
2025-03-17 12:06:17 -0400

    Update client-section.tsx

--- components/landing/client-section.tsx
+++ components/landing/client-section.tsx
@@ -7,7 +7,7 @@ export default function ClientSection() {
       <div className="py-14">
         <div className="mx-auto max-w-screen-xl px-4 md:px-8">
           <h2 className="text-center text-sm font-semibold text-gray-600">
-            TRUSTED BY TEAMS FROM AROUND THE WORLD
+            CONNECT ANYTHING TO EVERYTHING
           </h2>
           <div className="mt-6">
             <ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-white">
@@ -32,16 +32,116 @@ export default function ClientSection() {
 
               <li>
                 <img
-                  src={`https://cdn.magicui.design/companies/Uber.svg`}
+                  src={`https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg`}
                   className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
                 />
               </li>
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/f/f9/Salesforce.com_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/6/6e/Adobe_Corporate_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/5/57/ServiceNow_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/3/3f/HubSpot_Logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/b/b9/Slack_Technologies_Logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+
+                            <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/8/8a/Jira_Logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              
+              <li>
+                <img
+                  src={`https://cdn.magicui.design/companies/GitHub.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+                            
               <li>
                 <img
                   src={`https://cdn.magicui.design/companies/Notion.svg`}
                   className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
                 />
               </li>
+                            
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/sco/2/21/Nvidia_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              
+              <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/c/cc/Groq_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+
+                <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/commons/1/15/Cerebras_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              
+                <li>
+                <img
+                  src={`https://upload.wikimedia.org/wikipedia/sco/2/21/Nvidia_logo.svg`}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>
+              
+
+              
+              
             </ul>
           </div>
         </div>
