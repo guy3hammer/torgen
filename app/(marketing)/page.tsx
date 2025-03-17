4f954b630e05a12dad0ffa28ea77d5029ab0f98b
guy3hammer <guy3hammer@gmail.com>
2025-03-17 15:29:25 -0400

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,8 @@
 import Particles from "@/components/magicui/particles";
 import HeroSection from "@/components/landing/hero-section";
 import Hero2Section from "@/components/landing/hero2-section";
+import Hero3Section from "@/components/landing/hero3-section";
+import Hero4Section from "@/components/landing/hero4-section";
 import PricingSection from "@/components/landing/pricing-section";
 import ClientSection from "@/components/landing/client-section";
 import CallToActionSection from "@/components/landing/cta-section";
@@ -11,6 +13,8 @@ export default async function Page() {
     <>
       <HeroSection />
       <Hero2Section />
+      <Hero3Section />
+      <Hero4Section />
       <ClientSection />
       <SphereMask />
       <PricingSection />      
