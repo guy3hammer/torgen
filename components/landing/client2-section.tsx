3f7c26b5db5fbe79500cc7586bed9b5d39b0227e
guy3hammer <guy3hammer@gmail.com>
2025-03-19 12:04:05 -0400

    Update client2-section.tsx

--- components/landing/client2-section.tsx
+++ components/landing/client2-section.tsx
@@ -16,7 +16,8 @@ export default function ClientSection() {
           <div className="mt-6">
             <ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-white">
               
-
+            Test
+              Test
               
             </ul>
           </div>
