7d7cc6a745e69dfd2fed94609f8bf82699e3aa09
guy3hammer <guy3hammer@gmail.com>
2025-03-19 18:39:40 -0400

    Update client2-section.tsx

--- components/landing/client2-section.tsx
+++ components/landing/client2-section.tsx
@@ -1,4 +1,5 @@
 import TextShimmer from "@/components/magicui/text-shimmer";
+import { AvatarCircles } from "@/components/magicui/avatar-circles"
 
 export default function ClientSection() {
   return (
@@ -16,6 +17,41 @@ export default function ClientSection() {
             Torgen&apos;s <i>Open-Source-first</i> approach to AI transformation is grounded by a proven track record for innovation spanning finance, healthcare, 
             manufacturing, logistics, retail/e-comm, digital media and SaaS
 			</p>
+
+<div>
+const avatars = [
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/16860528",
+    profileUrl: "https://github.com/dillionverma",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/20110627",
+    profileUrl: "https://github.com/tomonarifeehan",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/106103625",
+    profileUrl: "https://github.com/BankkRoll",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/59228569",
+    profileUrl: "https://github.com/safethecode",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/59442788",
+    profileUrl: "https://github.com/sanjay-mali",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/89768406",
+    profileUrl: "https://github.com/itsarghyadas",
+  },
+]
+
+export default function AvatarCirclesDemo() {
+  return <AvatarCircles numPeople={99} avatarUrls={avatars} />
+}
+</div>
+
+		
 			<br></br>
 			<br></br>
 			<br></br>
