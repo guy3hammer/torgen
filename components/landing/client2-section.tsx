f816a073530dc383c486ae251daa5a48d0aa63d1
guy3hammer <guy3hammer@gmail.com>
2025-03-19 14:17:58 -0400

    Update client2-section.tsx

--- components/landing/client2-section.tsx
+++ components/landing/client2-section.tsx
@@ -8,80 +8,29 @@ export default function ClientSection() {
     >
       <div className="py-14">
         <div className="mx-auto max-w-screen-xl px-4 md:px-8">
-			<h3 className="text-5xl font-bold tracking-tight text-black dark:text-white sm:text-6xl">
-World class Resources
-			</h3>
+          <h2 className="text-5xl font-bold tracking-tight text-black dark:text-white sm:text-6xl">
+            Simple pricing for everyone.
+          </h2>
 
-			<p className="mt-6 text-xl leading-8 text-black/80 dark:text-white">
-				Torgen's <i>Open-Source-first</i> approach to AI transformation is driven by US citizens with a proven track record for innovation spanning finance, healthcare, manufacturing, logistics, retail/e-comm, digital media and SaaS
-			</p>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-			<div><h4 className="text-xl font-bold tracking-tight text-black dark:text-white">Flexible Solutions for Companies of All Sizes </h4>
-			</div>
-				
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Workshop/Bootcamp</strong></h2></span>
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>practical open source centric use case training for</i></span></p>
-				<p><span><i>technical and non-technical audiences</i></span></p>
-
-				<br></br>
-				<br></br>
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Strategy Consulting</strong></h2></span>
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>alignment with future state drivers, use case</i></span></p>
-				<p><span><i>productivity/cost-savings analysis, roadmap</i></span></p>
-				
-				<br></br>
-				<br></br>
-				
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Security Assessment</strong></h2></span>
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>data security, identity/access, logging, governance,</i></span></p>
-				<p><span><i>regulatory compliance, risk/gap analysis</i></span></p>
-
-				<br></br>
-				<br></br>
-				
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Project Consulting</strong></h2></span>
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>mobile/ai-first app dev; predictive modeling; data viz;</i></span></p>
-				<p><span><i>legacy code modernization, re-arch, re-platform, migration</i></span></p>
-
-				<br></br>
-				<br></br>
-				
-				<div><TextShimmer className="inline-flex items-center justify-center">
-				<span><h2><strong>AI Innovation Lab</strong></h2></span>
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>private managed AI testing environment for new model</i></span></p>
-				<p><span><i>benchmarking, use case testing and rapid prototyping</i></span></p>
-			</div>
-		</div> 	
-	</div>
-</section>
+          <p className="mt-6 text-xl leading-8 text-black/80 dark:text-white">
+            Choose an <strong>affordable plan</strong> that&apos;s packed with
+            the best features for engaging your audience, creating customer
+            loyalty, and driving sales.
+          </p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>         
+          <div className="mt-6">
+            <ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-white">
+              
+            Test
+              Test
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
