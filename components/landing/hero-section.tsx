8fddde8e44a81764a94afcfa141a3caa284eb515
guy3hammer <guy3hammer@gmail.com>
2025-03-21 13:39:46 -0400

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -108,6 +108,13 @@ export default function HeroSection() {
                   className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
                 />
               </li>
+
+              <li>
+                <img
+                  src={'/protectai-logo.svg'}
+                  className="h-8 w-28 px-2 dark:brightness-0 dark:invert"
+                />
+              </li>		    
 		    
             </ul>
           </div>
