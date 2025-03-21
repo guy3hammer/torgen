0abb18b81593e57751e37bf0906f7bc41555ec89
guy3hammer <guy3hammer@gmail.com>
2025-03-21 15:21:17 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 export const metadata: Metadata = {
-  title: "Magic UI",
-  description: "The startup template from Magic UI",
+  title: "Torgen AI",
+  description: "The Open Source AI Gurus",
 };
 
 export default function RootLayout({
