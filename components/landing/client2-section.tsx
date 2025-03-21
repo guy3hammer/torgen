5ba7d1b8bce3e7e20ff0c358f646554773eb4f05
guy3hammer <guy3hammer@gmail.com>
2025-03-21 16:57:42 -0400

    Update client2-section.tsx

--- components/landing/client2-section.tsx
+++ components/landing/client2-section.tsx
@@ -17,86 +17,10 @@ export default function ClientSection() {
 	
             <HyperText className="text-5xl font-bold tracking-tight text-black dark:text-white sm:text-6xl">
 		    World Class Resources</HyperText>
-		
-
-			<p className="mt-6 text-xl leading-8 text-black/80 dark:text-white">
-            Torgen&apos;s <i>Open-Source-first</i> approach to AI transformation is grounded by innovation track records spanning finance, healthcare, 
-            manufacturing, logistics, retail/e-comm, digital media and SaaS
-			</p>
-
-			<br></br>
-			<br></br>
-			<br></br>
-			<br></br>
-			<h4 className="text-xl font-bold tracking-tight text-black dark:text-white">
-            Flexible Solutions for Teams of All Sizes
-			</h4>
-
-				<br></br>
-				<br></br>
-				
-				<div>
-				<TextShimmer className="text-3xl font-bold tracking-tight sm:text-3xl">
-				OSAI Training Workshop
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>practical open source centric use case training for</i></span></p>
-				<p><span><i>technical and non-technical audiences</i></span></p>
-				<br></br>
-				<br></br>
-			
-				<div>
-				<TextShimmer className="text-3xl font-bold tracking-tight sm:text-3xl">
-				OSAI Strategy Consulting
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>alignment with future state drivers, use case</i></span></p>
-				<p><span><i>productivity/cost-savings analysis, roadmap</i></span></p>
-				<br></br>
-				<br></br>
-				
-				<div>
-				<TextShimmer className="text-3xl font-bold tracking-tight sm:text-3xl">
-				OSAI Security Assessment
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>data security, identity/access, logging, governance,</i></span></p>
-				<p><span><i>regulatory compliance, risk/gap analysis</i></span></p>
-				<br></br>
-				<br></br>
-				
-				<div>
-				<TextShimmer className="text-3xl font-bold tracking-tight sm:text-3xl">
-				OSAI Project Consulting
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>mobile/ai-first app dev; predictive modeling; data viz;</i></span></p>
-				<p><span><i>legacy code modernization, re-arch, re-platform, migration</i></span></p>
-				<br></br>
-				<br></br>
-				
-				<div>
-				<TextShimmer className="text-3xl font-bold tracking-tight sm:text-3xl">
-				OSAI Innovation Center
-				</TextShimmer>
-				</div>
-				
-				<br></br>
-				<p><span><i>private high-performance environment for OSAI model/tool</i></span></p>
-				<p><span><i>provisioning, tuning, testing and use case prototyping</i></span></p>
-				<br></br>
-				<br></br>
 				
         </div>
       </div>
     </section>
   );
 }
+
