1de7e341cff00d9e12445a6e80c3c9f70740b65e
guy3hammer <guy3hammer@gmail.com>
2025-03-21 17:08:08 -0400

    Update client2-section.tsx

--- components/landing/client2-section.tsx
+++ components/landing/client2-section.tsx
@@ -1,32 +1,6 @@
 import { HyperText } from "@/components/magicui/hyper-text";
 import { AvatarCircles } from "@/components/magicui/avatar-circles";
 
-const avatars = [
-  {
-    imageUrl: "https://avatars.githubusercontent.com/u/16860528",
-    profileUrl: "https://github.com/dillionverma",
-  },
-  {
-    imageUrl: "https://avatars.githubusercontent.com/u/20110627",
-    profileUrl: "https://github.com/tomonarifeehan",
-  },
-  {
-    imageUrl: "https://avatars.githubusercontent.com/u/106103625",
-    profileUrl: "https://github.com/BankkRoll",
-  },
-  {
-    imageUrl: "https://avatars.githubusercontent.com/u/59228569",
-    profileUrl: "https://github.com/safethecode",
-  },
-  {
-    imageUrl: "https://avatars.githubusercontent.com/u/59442788",
-    profileUrl: "https://github.com/sanjay-mali",
-  },
-  {
-    imageUrl: "https://avatars.githubusercontent.com/u/89768406",
-    profileUrl: "https://github.com/itsarghyadas",
-  },
-];
 
 export default function ClientSection() {
   return (
@@ -51,7 +25,4 @@ export default function ClientSection() {
   );
 }
 
-export default function AvatarCirclesDemo() {
-  return <AvatarCircles numPeople={99} avatarUrls={avatars} />;
-}
 
