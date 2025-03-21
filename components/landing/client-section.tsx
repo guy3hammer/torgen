fb8ffc8a48ec5cfbb0f21a10aae952be3bbb9b99
guy3hammer <guy3hammer@gmail.com>
2025-03-21 19:39:01 -0400

    Update client-section.tsx

--- components/landing/client-section.tsx
+++ components/landing/client-section.tsx
@@ -8,9 +8,7 @@ export default function ClientSection() {
         <div className="mx-auto max-w-screen-xl px-4 md:px-8">
 
           <br></br>
-          <br></br>
-          <br></br>
-          <br></br>         
+        
           <div className="mt-6">
             <ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-white">
               
