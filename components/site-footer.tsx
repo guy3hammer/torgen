26861a3b9c2cfafe2e17a42d95eb0f58aabd95be
guy3hammer <guy3hammer@gmail.com>
2025-03-22 12:05:23 -0400

    footer center

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,25 +4,13 @@ import Link from "next/link";
 export function SiteFooter() {
   return (
     <footer>
-      <div className="mx-auto w-full max-w-screen-xl xl:pb-2">
-        <div className="md:flex md:justify-between px-8 p-4 py-16 sm:pb-16 gap-4">
-          <div className="mb-12 flex-col flex gap-4">
-            <Link href="/" className="flex items-center gap-2">
+      <div className="center">
 
 	          <img
            		src={'/torgen-ai-logo.png'} 
         	  />
-              
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Torgen AI
-              </span>
-            </Link>
-            <p className="max-w-xs">Everyone wins when AI is Open Source</p>
-          </div>
-          </div>
-        </div>
 
-        <div className="flex flex-col sm:flex-row sm:flex sm:items-center sm:justify-between rounded-md border-neutral-700/20 py-4 px-8 gap-2">
+            <p className="max-w-xs">Everyone wins with Open Source AI</p>
 
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             Copyright © {new Date().getFullYear()}{" "}
